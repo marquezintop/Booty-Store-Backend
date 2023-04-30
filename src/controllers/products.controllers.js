9b0db5d46c2fcbc70955a7abbee2f2d8ad753c98
Guilfornari <guilfornari@gmail.com>
2023-04-30 15:58:56 -0300

    feat/get one meteorite implemented

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,4 +1,5 @@
 import { db } from "../database/database.connection.js";
+import { ObjectId } from "mongodb";
 
 export async function addMeteorite(req, res) {
     const { name } = req.body;
@@ -14,7 +15,7 @@ export async function addMeteorite(req, res) {
     }
 }
 
-export async function getMeteorites(req, res) {
+export async function getMeteorites(res) {
 
     try {
         const meteoritesList = await db.collection("meteorites").find().toArray();
@@ -24,4 +25,20 @@ export async function getMeteorites(req, res) {
         res.status(500).send(err.message);
     }
 }
+
+export async function getMeteorite(req, res) {
+    const { id } = req.params;
+
+    try {
+        const meteorite = await db.collection("meteorites").findOne({ _id: new ObjectId(id) });
+        console.log(meteorite);
+        res.status(200).send(meteorite);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+
+
+
 
