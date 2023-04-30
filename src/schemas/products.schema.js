302fa9430efca58ba8bb13e8b276027ae7bd1916
Guilfornari <guilfornari@gmail.com>
2023-04-30 14:09:41 -0300

    feat/products post products to db and get products, forgot to change branch...

---
+++ src/schemas/products.schema.js
@@ -0,0 +1,11 @@
+import joi from "joi";
+
+export const meteoriteSchema = joi.object({
+    name: joi.string().required(),
+    date: joi.date().raw().required(),
+    description: joi.string().required(),
+    location: joi.string().required(),
+    picture: joi.string().uri().required(),
+    fullDescription: joi.string().required(),
+    price: joi.number().precision(2).required()
+});
