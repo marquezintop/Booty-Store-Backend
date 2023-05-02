6487e80e1bce444f0720a1ca7dcca112ad6fffd3
Guilfornari <guilfornari@gmail.com>
2023-05-01 23:58:57 -0300

    changes to use deplyed database

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import productsRouter from "./routes/products.routes.js";
 import cartRouter from "./routes/cart.routes.js";
+import dotenv from "dotenv";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(authRouter);
-server.use(productsRouter);4
+server.use(productsRouter); 4
 server.use(cartRouter);
 
-const PORT = 5000;
-server.listen(PORT, () => console.log("Server is running on port: " + PORT));
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log("Server is running on port: " + port));
